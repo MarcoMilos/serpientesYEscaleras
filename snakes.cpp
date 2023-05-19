8518ab3e074ebc6b3aa4ef3da79f53c9584597a8
MarcoMilos <marcomiloslavich03@gmail.com>
2023-05-19 12:04:09 -0600

    Add snakes.cpp

---
+++ snakes.cpp
@@ -0,0 +1,12 @@
+// Marco & Alejandro Miloslavich Airola (A01424280, A01424120)
+// 19-05-2023
+// Serpientes y Escaleras
+
+#include "MyGame.h"
+
+int main()
+{
+    MyGame g;
+    g.start();
+    return 0;
+}
