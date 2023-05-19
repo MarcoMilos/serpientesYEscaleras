aef794be2ca8cb5131601266c98d7e98f808b6fa
alejandroMilos <alejandromilospersonal@gmail.com>
2023-05-19 15:05:11 -0600

    Add class_Player2

--- player2.h
+++ player2.h
@@ -1,3 +1,29 @@
 // Marco & Alejandro Miloslavich Airola (A01424280, A01424120)
 // Started in: 19-05-2023
 // Serpientes y Escaleras
+
+#ifdef player2_h
+#define player2_h
+
+class Player2
+{
+    int position;
+
+    public:
+        Player2()
+        {
+            position = 1;
+        }
+
+        int getPosition() const
+        {
+            return position;
+        }
+
+        void setPosition(int newPosition)
+        {
+            position = newPosition;
+        }
+};
+
+#endif player2_h
