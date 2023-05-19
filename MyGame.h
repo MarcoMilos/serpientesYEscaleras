fcbbc2e15b7905650f0f862d46547d9db27ea8c8
MarcoMilos <marcomiloslavich03@gmail.com>
2023-05-19 12:03:59 -0600

    Add MyGame.h

---
+++ MyGame.h
@@ -0,0 +1,16 @@
+// Marco & Alejandro Miloslavich Airola (A01424280, A01424120)
+// 19-05-2023
+// Serpientes y Escaleras
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+class MyGame
+{
+    public:
+        void start()
+        {
+            // Lógica del juego
+        }
+};  
